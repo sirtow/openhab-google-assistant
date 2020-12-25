75c9b422fec08c4d5dff2e8d44a659d4e5e9476f
sirtow <sirtow@gmail.com>
2020-12-24 20:03:54 -0500

    request logging

--- testServer.js
+++ testServer.js
@@ -4,6 +4,13 @@ const bodyParser = require('body-parser')
 const openhabGA = require('./functions/index.js');
 
 app.use(bodyParser.json());
+app.use(function( req, res, next) {
+  console.log("vvvvvvvvvvvv")
+  console.log(req.headers)
+  console.log(req.body)
+  console.log("^^^^^^^^^^^^")
+  next()
+});
 
 app.use('/', (req, res) => {
   openhabGA.openhabGoogleAssistant(req, res);
